8357e252521e333f0cb8f1a8703be1f17eb0ccce
JoeTheorium <fernandoandresraggio@gmail.com>
2023-05-31 21:42:12 -0300

    New name, new deploy preparation

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ReactJS-OpenBootcamp/"
+  base: "/ReactJS-Sesion3-OpenBootcamp/"
 });
